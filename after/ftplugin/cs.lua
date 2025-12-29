ef6de3d12dcab3e4fc64d033c8b286b43bc5858b
Charles Taylor <charlestaylor95@gmail.com>
2025-12-29 17:50:55 -0500

    Virtual visibility

---
+++ after/ftplugin/cs.lua
@@ -0,0 +1,136 @@
+local visibilities = { "internal", "public", "protected", "private", "file" }
+
+---@param node TSNode
+local function has_explicit_visibility(node)
+  return vim.iter(node:iter_children()):any(function(child)
+    if child:type() ~= "modifier" then
+      return false
+    end
+    local a, b, c, d = child:range()
+    local modifier = vim.api.nvim_buf_get_text(0, a, b, c, d, {})[1]
+    return vim.tbl_contains(visibilities, modifier)
+  end)
+end
+
+local function get_root_node(lang, bufnr)
+  return vim.treesitter.get_parser(bufnr or 0, lang):parse()[1]:root()
+end
+
+local function show_virtual_visibility()
+  local ns = vim.api.nvim_create_namespace("csharp:virtual-text")
+  vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+  vim.api.nvim_set_hl_ns(ns)
+  vim.api.nvim_set_hl(ns, "VirtualVisibility", { fg = 13346551, bg = "black" })
+
+  local doc = get_root_node("c_sharp")
+
+  local query_local_functions = vim.treesitter.query.parse("c_sharp", [[ (local_function_statement) @local ]])
+
+  for id, node, meta, match in query_local_functions:iter_captures(doc, 0) do
+    local a, b, c, d = node:range()
+    vim.api.nvim_buf_set_extmark(0, ns, a, b, {
+      right_gravity = false,
+      virt_text_pos = "inline",
+      virt_text = { { "local", "VirtualVisibility" }, { " " } },
+    })
+  end
+
+  local query_members = vim.treesitter.query.parse(
+    "c_sharp",
+    [[
+        [
+            (field_declaration)
+            (property_declaration)
+            (method_declaration)
+            (event_field_declaration)
+        ] @member
+    ]]
+  )
+  for id, node, meta, match in query_members:iter_captures(doc, 0) do
+    if not has_explicit_visibility(node) then
+      local visibility = "private"
+      if node:parent():parent():type() == "interface_declaration" then
+        visibility = "public"
+      end
+
+      local first_non_attribute_child = vim.iter(node:iter_children()):find(function(child)
+        return child:type() ~= "attribute_list"
+      end)
+      if first_non_attribute_child == nil then
+        break
+      end
+
+      local a, b, _, _ = first_non_attribute_child:range()
+
+      vim.api.nvim_buf_set_extmark(0, ns, a, b, {
+        right_gravity = false,
+        virt_text_pos = "inline",
+        virt_text = { { visibility, "VirtualVisibility" }, { " " } },
+      })
+    end
+  end
+
+  local query_internal_types = vim.treesitter.query.parse(
+    "c_sharp",
+    [[
+        [
+            (class_declaration)
+            (struct_declaration)
+            (interface_declaration)
+            (record_declaration)
+            (enum_declaration)
+            (delegate_declaration)
+        ] @type
+    ]]
+  )
+
+  for id, node, meta, match in query_internal_types:iter_captures(doc, 0) do
+    if not has_explicit_visibility(node) then
+      local first_non_attribute_child = vim.iter(node:iter_children()):find(function(child)
+        return child:type() ~= "attribute_list"
+      end)
+      if first_non_attribute_child == nil then
+        break
+      end
+
+      local a, b, _, _ = first_non_attribute_child:range()
+      vim.api.nvim_buf_set_extmark(0, ns, a, b, {
+        right_gravity = false,
+        virt_text_pos = "inline",
+        virt_text = { { "internal", "VirtualVisibility" }, { " " } },
+      })
+    end
+  end
+
+  local query_constructors = vim.treesitter.query.parse(
+    "c_sharp",
+    [[
+        (constructor_declaration) @method
+    ]]
+  )
+  for id, node, meta, match in query_constructors:iter_captures(doc, 0) do
+    if not has_explicit_visibility(node) then
+      local first_non_attribute_child = vim.iter(node:iter_children()):find(function(child)
+        return child:type() ~= "attribute_list"
+      end)
+      if first_non_attribute_child == nil then
+        break
+      end
+      local a, b, _, _ = first_non_attribute_child:range()
+
+      vim.api.nvim_buf_set_extmark(0, ns, a, b, {
+        right_gravity = false,
+        virt_text_pos = "inline",
+        virt_text = { { "public", "VirtualVisibility" }, { " " } },
+      })
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = vim.api.nvim_create_augroup("csharp:virtual-visibility", {}),
+  buffer = 0,
+  callback = show_virtual_visibility,
+})
+
+vim.schedule(show_virtual_visibility)
